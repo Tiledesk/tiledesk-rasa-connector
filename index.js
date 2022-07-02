5ef711f2b7d4291ab6b78bd47827116ae521a179
Andrea Leo <andrealeo83@users.noreply.github.com>
2022-07-02 09:45:22 +0000

    first release ok, added option for log, added option for default chatbotInfo

--- index.js
+++ index.js
@@ -2,16 +2,28 @@ require('dotenv').config();
 var express = require('express');
 
 var app = express();
-const rasaroute = require("./rasaRoute.js");
-rasaroute.start(
+const rasa = require("./rasaRoute.js");
+
+const rasaRouter = rasa.router;
+app.use("/rasa", rasaRouter);
+
+rasa.startRasa(
   {
     KVBASE_COLLECTION : process.env.KVBASE_COLLECTION,
-    MONGODB_URI: process.env.MONGODB_URI
+    MONGODB_URI: process.env.MONGODB_URI,
+    chatbotInfo: {
+      serverUrl: 'http://52.16.50.206/webhooks/rest/webhook'
+    },
+    log: false
   }, () => {
-    app.use("/rasa", rasaroute);
+    console.log("RASA route successfully started.");
+    var port = process.env.PORT || 3000;
+    app.listen(port, function () {
+      console.log('RASA connector listening on port ', port);
+    });
   });
 
 app.get('/', (req, res) => {
-  res.write("Hello from RASA connector 0.1.0");
+  res.write("Hello from RASA connector");
   res.end();
 });
--- rasaRoute.js
+++ rasaRoute.js
@@ -20,6 +20,8 @@ router.use(cors());
 router.use(bodyParser.json({limit: '50mb'}));
 router.use(bodyParser.urlencoded({ extended: true , limit: '50mb'}));
 
+let chatbotInfo = null;
+let log = true;
 // const structjson = require('./structJson.js');
 
 // async function getChatbotData(chatbot_id, callback) {
@@ -30,7 +32,9 @@ router.use(bodyParser.urlencoded({ extended: true , limit: '50mb'}));
 // }
 
 function runRASAQuery(RASAurl, rasa_sender_id, text, callback) {
-
+  if (log) {
+    console.log("Using RASA URL:", RASAurl)
+  }
   request(
     {
       url: `${RASAurl}`,
@@ -44,7 +48,9 @@ function runRASAQuery(RASAurl, rasa_sender_id, text, callback) {
       }
     },
     function(err, res, resbody) {
-      console.log("res.statusCode:", res.statusCode)
+      if (log) {
+        console.log("res.statusCode:", res.statusCode)
+      }
       if (err) {
         console.log("An error occurred", err);
       }
@@ -52,13 +58,16 @@ function runRASAQuery(RASAurl, rasa_sender_id, text, callback) {
         console.log("status code error: res.statusCode = ", res.statusCode);
       }
       else {
-        console.log("RASA REPLY:", resbody);
+        if (log) {
+          console.log("RASA REPLY:", resbody);
+        }
         callback(resbody);
       }
     }
   );
 }
 
+/*
 function sendMessage(msg_json, project_id, recipient, token, callback) {
   console.log("Sending message to Tiledesk: " + JSON.stringify(msg_json));
   request({
@@ -74,12 +83,14 @@ function sendMessage(msg_json, project_id, recipient, token, callback) {
       callback(err)
     }
   );
-}
+}*/
 
 router.post("/rasabot", async (req, res) => {
   // delete req.body.payload.request.messages;
-  console.log(" ******* NEW REQUEST *******\n\n\n")
-  console.log('req.body:', JSON.stringify(req.body));
+  if (log) {
+    console.log(" ******* NEW REQUEST *******\n\n\n")
+    console.log('req.body:', JSON.stringify(req.body));
+  }
   // console.log("BOT: req.body: " + JSON.stringify(req.body));
   // const project_id = req.body.payload.id_project;
   // const channel = req.body.payload.channel;
@@ -91,9 +102,10 @@ router.post("/rasabot", async (req, res) => {
   
   var _API_URL = process.env.API_ENDPOINT
   const cbclient = new TiledeskChatbotClient({request: req, response: res, APIURL: _API_URL, APIKEY: '____APIKEY____', log: API_LOG});
-
-  console.log("cbclient.APIURL", cbclient.APIURL)
-  console.log("cbclient.tiledeskClient.APIURL",   cbclient.tiledeskClient.APIURL)
+  if (log) {
+    console.log("cbclient.APIURL", cbclient.APIURL)
+    console.log("cbclient.tiledeskClient.APIURL",   cbclient.tiledeskClient.APIURL);
+  }
   
   var chatbot_id;
   var chatbot_name;
@@ -101,9 +113,10 @@ router.post("/rasabot", async (req, res) => {
     chatbot_name = cbclient.chatbot_name; // const chatbot_name = req.body.hook.name;
     // console.log("chatbot_id from body:", chatbot_id)
   // }
-  console.log("CHATBOT-ID: ", chatbot_id)
-
-  console.log(" ******* TEXT *******" + cbclient.text)
+  if (log) {
+    console.log("CHATBOT-ID: ", chatbot_id)
+    console.log(" ******* TEXT *******" + cbclient.text)
+  }
   // RASA Tiledesk payload: HOW TO USE IT?
   let payload = {}
   payload.tiledesk = req.body;
@@ -118,17 +131,23 @@ router.post("/rasabot", async (req, res) => {
       "first_text": cbclient.text
     }
     cbclient.tiledeskClient.updateRequestProperties(conversation.request_id, properties, function(err) {
-      console.log("request updated with text: ", cbclient.text)
+      if (log) {
+        console.log("request updated with text: ", cbclient.text)
+      }
     });
   }
 
   const rasa_user_id = conversation.request_id;
-  console.log("looking for chatbot", chatbot_id)
-  const chatbotInfo = await db.get(chatbot_id);
-  console.log("Chatbot found!", chatbotInfo);
+  if (!chatbotInfo) {
+    if (log) {console.log("looking for chatbot", chatbot_id)}
+    chatbotInfo = await db.get(chatbot_id);
+    if (log) {console.log("Chatbot found!", chatbotInfo);}
+  }
   const RASAurl = chatbotInfo.serverUrl;
   runRASAQuery(RASAurl, rasa_user_id, cbclient.text, function(result) {
-    console.log("BOT: RASA REPLY: " + JSON.stringify(result));
+    if (log) {
+      console.log("BOT: RASA REPLY: " + JSON.stringify(result));
+    }
     if(res.statusCode === 200) {
       if (result && result.length > 0 && result[0].text) {
         cbclient.tiledeskClient.sendSupportMessage(
@@ -137,7 +156,9 @@ router.post("/rasabot", async (req, res) => {
             text: result[0].text
           },
           () => {
-            console.log("Message sent.");
+            if (log) {
+              console.log("Message sent.");
+            }
           }
         );
       }
@@ -157,9 +178,11 @@ function sendBackToTiledesk(cbclient, payload, result) {
   const is_fallback = result.intent.isFallback;
   const intent_confidence = result.intentDetectionConfidence;
   const intent_name = result.intent.displayName;
-  console.log("result.intent.isFallback?", is_fallback);
-  console.log(`result.intent.displayName: ${intent_name}`);
-  console.log(`Confidence: ${intent_confidence}`);
+  if (log) {
+    console.log("result.intent.isFallback?", is_fallback);
+    console.log(`result.intent.displayName: ${intent_name}`);
+    console.log(`Confidence: ${intent_confidence}`);
+  }
   // const payload_clone = Object.assign({}, payload);
   // const message_object = delete payload_clone.request;
   const intent_info = {
@@ -175,6 +198,7 @@ function sendBackToTiledesk(cbclient, payload, result) {
   // }
 }
 
+/*
 function fireNotFoundEvent(cbclient, is_fallback, confidence) {
   const event = {
     name: "faqbot.answer_not_found",
@@ -202,14 +226,19 @@ function fireNotFoundEvent(cbclient, is_fallback, confidence) {
       console.log("EVENT FIRED:", result);
     }
   });
-}
+}*/
 
 router.post('/botcredendials/:project/bots/:chatbot', (req, res) => {
   const chatbot_id = req.params.chatbot;
-  console.log("post bot credentials for", chatbot_id)
-  console.log('req.body:', JSON.stringify(req.body));
+  if (log) {
+    console.log("post bot credentials for", chatbot_id)
+    console.log('req.body:', JSON.stringify(req.body));
+  }
   const data = req.body;
-  console.log("DARA::", data)
+  if (log) {
+    console.log("DATA:", data)
+  }
+  
   //var form = new formidable.IncomingForm();
   // FORM.PARSE in advance: to avoid H18 error
   // Our 503s (H18 errors) were caused by us not consuming POST data from the socket for some requests.
@@ -218,12 +247,16 @@ router.post('/botcredendials/:project/bots/:chatbot', (req, res) => {
     //console.log("Form parsed.")
     verifyAuthorization(req, function(verified) {
       if (!verified) {
-        console.log("Post Unauthorized.")
+        if (log) {
+          console.log("Post Unauthorized.")
+        }
         res.status(403).send({success: false, msg: 'Unauthorized'});
         return
       }
       else {
-        console.log("Post Authorized.")
+        if (log) {
+          console.log("Post Authorized.")
+        }
         updateCredentials(chatbot_id, data, res, () => {
           //res.writeHead(200, {'content-type': 'application/json'});
           res.send({"success": true});
@@ -234,40 +267,58 @@ router.post('/botcredendials/:project/bots/:chatbot', (req, res) => {
 });
 
 async function updateCredentials(chatbot_id, data, res, callback) {
-  console.log("updating chatbot: ", chatbot_id)
+  if (log) {
+    console.log("updating chatbot: ", chatbot_id)
+  }
   const value = await db.get(chatbot_id);
-  console.log("chatbot found: ", value);
+  if (log) {
+    console.log("chatbot found: ", value);
+  }
   let content = {}
   if (value) {
     content = value;
   }
   if (data && data.serverUrl && data.serverUrl.trim() != '') {
     content.serverUrl = data.serverUrl
-    console.log("value.serverUrl updated.");
+    if (log) {
+      console.log("value.serverUrl updated.");
+    }
   }
   else {
     content.serverUrl = null;
   }
-  console.log("saving content: ", JSON.stringify(content));
+  if (log) {
+    console.log("saving content: ", JSON.stringify(content));
+  }
   await db.set(chatbot_id, content);
-  console.log("data saved.");
+  if (log) {
+    console.log("data saved.");
+  }
   callback();
 }
 
 router.get('/botcredendials/:project/bots/:chatbot', (req, res) => {
   verifyAuthorization(req, async (verified) => {
     if (!verified) {
-      console.log("Get Unauthorized.")
+      if (log) {
+        console.log("Get Unauthorized.")
+      }
       res.status(403).send({success: false, msg: 'Unauthorized.'});
     }
     else {
-      console.log("Get Authorized.")
+      if (log) {
+        console.log("Get Authorized.")
+      }
       const chatbot_id = req.params.chatbot;
-      console.log("getting chatbot: ", chatbot_id)
+      if (log) {
+        console.log("getting chatbot: ", chatbot_id)
+      }
       let response = {};
       try {
         const reply = await db.get(chatbot_id);
-        console.log("reply:", reply)
+        if (log) {
+          console.log("reply:", reply)
+        }
         if (!reply) {
           response.success = false
           response.errMessage = "No bot found"
@@ -300,12 +351,16 @@ router.get('/botcredendials/:project/bots/:chatbot', (req, res) => {
 router.delete('/botcredendials/:project/bots/:chatbot', async (req, res) => {
   verifyAuthorization(req, async (verified) => {
     if (!verified) {
-      console.log("Delete Unauthorized.")
+      if (log) {
+        console.log("Delete Unauthorized.")
+      }
       res.status(403).send({success: false, msg: 'Unauthorized'});
     }
     else {
       const chatbot_id = req.params.chatbot;
-      console.log("deleting chatbot: ", chatbot_id)
+      if (log) {
+        console.log("deleting chatbot: ", chatbot_id)
+      }
       await db.remove(chatbot_id);
       res.status(200).send({success: true});
       res.end();
@@ -316,26 +371,38 @@ router.delete('/botcredendials/:project/bots/:chatbot', async (req, res) => {
 function verifyAuthorization(req, callback) {
   const chatbot_id = req.params.chatbot;
   const project_id = req.params.project;
-  console.log("veryfing user...", JSON.stringify(req.headers))
+  if (log) {
+    console.log("veryfing user...", JSON.stringify(req.headers))
+  }
   var token = getToken(req.headers);
-  console.log("got token: ", token)
+  if (log) {
+    console.log("got token: ", token)
+  }
   if (!token) {
-    console.log("No token provided.")
+    if (log) {
+      console.log("No token provided.")
+    }
     callback(false)
     return
   }
   getBotDetail(project_id, chatbot_id, token, function(err, bot) {
-    console.log("getBotDetail ok", err, bot)
+    if (log) {
+      console.log("getBotDetail ok", err, bot)
+    }
     if (err) {
-      console.log("getBotDetail err", err)
+      console.error("getBotDetail err", err)
       callback(false)
     }
     else if (bot && bot.success && bot.success == false) {
-      console.log("getBotDetail bot && bot.success && bot.success == false")
+      if (log) {
+        console.log("getBotDetail bot && bot.success && bot.success == false")
+      }
       callback(false)
     }
     else {
-      console.log("getBotDetail true")
+      if (log) {
+        console.log("getBotDetail true")
+      }
       callback(true)
     }
   })
@@ -344,7 +411,9 @@ function verifyAuthorization(req, callback) {
 function getBotDetail(project_id, chatbot_id, token, callback) {
   // e.g. https://tiledesk-server-pre.herokuapp.com/5e51984fc9c41700175e165c/faq_kb/5e519889c9c41700175e1660
   const URL = `${process.env.API_ENDPOINT}/${project_id}/faq_kb/${chatbot_id}`
-  console.log("getBotDetail URL:", URL)
+  if (log) {
+    console.log("getBotDetail URL:", URL)
+  }
   request({
     url: URL,
     headers: {
@@ -359,7 +428,9 @@ function getBotDetail(project_id, chatbot_id, token, callback) {
         callback("Unauthorized", null);
       }
       else {
-        console.log("getBotDetail.resbody: ", resbody)
+        if (log) {
+          console.log("getBotDetail.resbody:", resbody)
+        }
         callback(null, resbody);
       }
     }
@@ -375,17 +446,25 @@ function getToken(headers) {
 }
 
 function startRasa(settings, completionCallback) {
+  
+  console.log("Starting RASA with Settings:", settings);
   if (!settings.MONGODB_URI) {
     throw new Error("settings.MONGODB_URI is mandatory.");
   }
+  if (settings.chatbotInfo) {
+    chatbotInfo = settings.chatbotInfo;
+  }
+  if (!settings.log) {
+    log = false;
+  }
   console.log("Starting RASA connector...");
-  console.log("Connecting to mongodb...");
-  console.log("settings.KVBASE_COLLECTION:", settings.KVBASE_COLLECTION);
+  console.log("(RASA) Connecting to mongodb...");
+  console.log("(RASA) settings.KVBASE_COLLECTION:", settings.KVBASE_COLLECTION);
   const kvbase_collection = settings.KVBASE_COLLECTION ? settings.KVBASE_COLLECTION : 'kvstore';
-  console.log("kvbase_collection:", kvbase_collection);
+  console.log("(RASA) kvbase_collection:", kvbase_collection);
   db = new KVBaseMongo(kvbase_collection);
-  db.connect(MONGODB_URI, () => {
-    console.log("RASA KVBaseMongo successfully connected.");
+  db.connect(settings.MONGODB_URI, () => {
+    console.log("(RASA) KVBaseMongo successfully connected.");
     if (completionCallback) {
       completionCallback();
     }
